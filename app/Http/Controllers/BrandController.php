4d2f1e32244e70e118cf4a908c6c7a09177d3883
Waqar Nawaz N <wordpresslover7@gmail.com>
2025-12-17 00:12:03 +0500

    Update BrandController.php

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -646,6 +646,11 @@ class BrandController extends Controller
         session(['selected_brand_id' => $brand->id]);
 
         $brand->load([
+            'prompts' => function ($query) {
+                $query->where('is_active', true)
+                    ->orderByDesc('analysis_completed_at')
+                    ->orderByDesc('updated_at');
+            },
             'prompts.promptResources',
             'prompts.aiModel',
             'subreddits', 
